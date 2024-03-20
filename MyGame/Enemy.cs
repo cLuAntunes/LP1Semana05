6123f0cd11e765555582241f528b50f4cbf2348a
Clara Antunes <claraantunes.work@gmail.com>
2024-03-20 18:57:31 +0000

    Add GetName and TakeDamage methods

--- MyGame/Enemy.cs
+++ MyGame/Enemy.cs
@@ -5,13 +5,35 @@ namespace MyGame
         private string name;
         private float health;
         private float shield; 
-        
+
         public Enemy (string name)
         {
             this.name = name;
             health = 100;
             shield = 0;
         }
+
+        public string GetName()
+        {       
+            return name;
+        }
+
+        public void TakeDamage(float damage)
+        {   
+            shield -= damage;
+
+            if (shield < 0)
+            {
+                float damageStillToInflict = -shield;
+                shield = 0;
+                health -= damageStillToInflict;
+        
+            if (health < 0)
+                health = 0;
+            }
+        }
+
+
         
     }
 }
