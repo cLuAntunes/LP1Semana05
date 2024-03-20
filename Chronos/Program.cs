5966881095403eb20ca5809b334689860bb7f157
Clara Antunes <claraantunes.work@gmail.com>
2024-03-20 17:28:04 +0000

    Start and Pause instances crono 1 and crono 2

---
+++ Chronos/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading; 
+
+namespace Chronos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch crono1 = new Stopwatch();
+            Stopwatch crono2 = new Stopwatch();
+
+            crono1.Start();
+            Thread.Sleep(600);
+
+            crono2.Start();
+            Thread.Sleep(200);
+            
+        }
+    }
+}
