cc395f52e55901495a739e6e6c99a7b7441be99f
Clara Antunes <claraantunes.work@gmail.com>
2024-03-21 13:20:33 +0000

    Add Enemy array

--- MyGame/Program.cs
+++ MyGame/Program.cs
@@ -6,7 +6,19 @@ namespace MyGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int enemies = int.Parse(args[0]);
+
+            Enemy[] allenemies = new Enemy[enemies];
+
+            for (int i = 0; i < enemies; i++)
+            {
+                Console.Write($"Nome do inimigo {i + 1}: ");
+                string name = Console.ReadLine();
+                allenemies[i] = new Enemy(name);
+            }
+
+        
+
         }
     }
 }
